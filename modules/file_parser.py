0a90001ff978a2ba92f8646c097c37338813f24e
cr0iss4nt <809dan4ik@gmail.com>
2026-02-25 18:26:27 +0300

    Added docx and pdf parsing

--- modules/file_parser.py
+++ modules/file_parser.py
@@ -1,6 +1,7 @@
 from striprtf.striprtf import rtf_to_text
 from pathlib import Path
-
+import pdfplumber
+import docx2txt  # add import for docx parsing
 
 def parse_txt(filename):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -13,9 +14,27 @@ def parse_rtf(filename):
         text = rtf_to_text(content)
         return text
 
+def parse_pdf(filename):
+    text = ''
+    with pdfplumber.open(filename) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + '\n'
+    return text
+
+def parse_docx(filename):
+    text = docx2txt.process(filename)
+    return text
+
 def parse_file(filename):
     path = Path(filename)
     if path.suffix == '.rtf':
         return parse_rtf(filename)
     if path.suffix == '.txt':
         return parse_txt(filename)
+    if path.suffix == '.pdf':
+        return parse_pdf(filename)
+    if path.suffix == '.docx':
+        return parse_docx(filename)
+    return ""
